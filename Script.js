a8edf2eeacfc3ef99e8f70447f60f75a2d40f763
aminmarandi <137484939+aminmarandi@users.noreply.github.com>
2023-08-10 20:04:12 -0700

    navigation

--- Script.js
+++ Script.js
@@ -1,29 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-  // Event listener for button click
-  let buttons = document.querySelectorAll(".button");
-  for (let button of buttons) {
-    button.addEventListener("click", function() {
-      // Toggle color between gray and primary color
-      if (this.style.backgroundColor === "var(--dl-color-gray-white)") {
-        this.style.backgroundColor = "var(--dl-color-primary-300)";
-        this.style.color = "var(--dl-color-gray-white)";
-      } else {
-        this.style.backgroundColor = "var(--dl-color-gray-white)";
-        this.style.color = "var(--dl-color-gray-black)";
-      }
-    });
-  }
+<script>
+  const navToggle = document.getElementById("navToggle");
+  const navMenuContainer = document.querySelector(".nav-menu-container");
 
-  // Event listener for input and textarea focus
-  let inputs = document.querySelectorAll(".input, .textarea");
-  for (let input of inputs) {
-    input.addEventListener("focus", function() {
-      this.style.borderColor = "var(--dl-color-primary-300)";
-    });
-    input.addEventListener("blur", function() {
-      this.style.borderColor = "var(--dl-color-gray-black)";
-    });
-  }
-
-});
+  navToggle.addEventListener("click", () => {
+    navMenuContainer.classList.toggle("active");
+  });
+</script>
